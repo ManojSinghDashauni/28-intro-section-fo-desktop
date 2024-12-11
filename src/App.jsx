a02e8613b7fd76d706f5d94f5b0147c1ab44ea04
ManojSinghDashauni <dashauni007@gmail.com>
2024-12-11 15:08:25 +0530

    last commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Home from "./Home";
+import Navigation from "./Navigation";
+
+export default function App() {
+  return (
+    <div className="w-screen h-screen">
+      <Navigation />
+      <Home />
+    </div>
+  );
+}
